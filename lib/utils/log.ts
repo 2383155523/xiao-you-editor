44b92dbff0ce4973566d6858078415008f35d408
微若蜉蝣 <2383155523@qq.com>
2022-10-27 22:01:58 +0800

    release: v0.1.3

--- lib/utils/log.ts
+++ lib/utils/log.ts
@@ -7,7 +7,7 @@ const XiaoYouLog = () => {
     "\n" +
       " %c xiao-you-editor v" +
       pkg.version +
-      ` %c  https://2383155523.github.io/xiao-you-editor-docs/ ` +
+      ` %c  https://fuyouplus.gitee.io/xiao-you-editor-docs/ ` +
       "\n",
     "color: #E5E7EB; background: #6739b6; padding:5px 0;",
     "background: #333; padding:5px 0;"
